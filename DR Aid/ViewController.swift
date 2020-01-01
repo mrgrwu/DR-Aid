2cc49b94707f4890a196140ff39bcb7a4fcaede6
Greg Wu <grwu@yahoo.com>
2020-01-01 01:35:15 -0800

    Initial Commit

---
+++ DR Aid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DR Aid
+//
+//  Created by Greg Wu on 1/1/20.
+//  Copyright © 2020 Greg Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
